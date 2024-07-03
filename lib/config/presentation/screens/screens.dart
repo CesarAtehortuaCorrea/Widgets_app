6ba5b2bff9598ba4f732b4de69bdf4d5887f239c
Cesar Atehortua <zergux@gmail.com>
2024-07-03 08:19:03 -0500

    Barril ok con pantallas

--- lib/config/presentation/screens/screens.dart
+++ lib/config/presentation/screens/screens.dart
@@ -7,4 +7,3 @@ export 'package:widgets_app/config/presentation/screens/ui_controls/ui_controls_
 export 'package:widgets_app/config/presentation/screens/buttons/buttons_screen.dart';
 export 'package:widgets_app/config/presentation/screens/cards/cards_screen.dart';
 export 'package:widgets_app/config/presentation/screens/home/home_screen.dart';
-
